3ba1721f03adb8b09ffdd228a3a1d98403459370
Lior Galanti <lior.galanti@amd.com>
2025-05-28 20:40:56 +0000

    thrust::null_type seems to still be an option on our aging thrust

--- cpp/include/cugraph/utilities/thrust_tuple_utils.hpp
+++ cpp/include/cugraph/utilities/thrust_tuple_utils.hpp
@@ -120,7 +120,7 @@ template <typename... Ts>
 struct is_thrust_tuple_of_arithmetic<thrust::tuple<Ts...>> {
  private:
   template <typename T>
-  static constexpr bool is_valid = std::is_arithmetic_v<T>;
+  static constexpr bool is_valid = std::is_arithmetic_v<T> || std::is_same_v<T, thrust::null_type>;
 
  public:
   static constexpr bool value = (... && is_valid<Ts>);
@@ -133,7 +133,7 @@ template <typename... Ts>
 struct is_thrust_tuple_of_integral<thrust::tuple<Ts...>> {
  private:
   template <typename T>
-  static constexpr bool is_valid = std::is_integral_v<T>;
+  static constexpr bool is_valid = std::is_integral_v<T> || std::is_same_v<T, thrust::null_type>;
 
  public:
   static constexpr bool value = (... && is_valid<Ts>);
