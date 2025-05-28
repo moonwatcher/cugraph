75e78876931dd8580b61f6a2aea8f36a4fe6c809
Lior Galanti <lior.galanti@amd.com>
2025-05-28 00:38:14 +0000

    compute_hash expects std::byte

--- cpp/src/structure/remove_multi_edges_impl.cuh
+++ cpp/src/structure/remove_multi_edges_impl.cuh
@@ -56,7 +56,7 @@ struct hash_src_dst_pair {
     pair[0] = thrust::get<0>(t);
     pair[1] = thrust::get<1>(t);
     cuco::murmurhash3_32<vertex_t*> hash_func{};
-    return hash_func.compute_hash(reinterpret_cast<cuda::std::byte*>(pair), 2 * sizeof(vertex_t)) %
+    return hash_func.compute_hash(reinterpret_cast<std::byte*>(pair), 2 * sizeof(vertex_t)) %
            num_groups;
   }
 };
